944732951641e23ae705f45bff78f488841767aa
WendyNdlalane <wendy.ndlalane@gmail.com>
2023-07-27 23:39:47 +0200

    build fix

--- app/Controllers/Http/AnalyticsController.ts
+++ app/Controllers/Http/AnalyticsController.ts
@@ -1,17 +1,17 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import AnalyticsSchema from 'App/Models/AnalyticsSchema'
-import sha1 from 'sha1'
+
 
 export default class AnalyticsController {
 
- function detailAnalytics(body,callback) {
+ function detailAnalytics(body) {
 
   const analytics = await  AnalyticsSchema.findBy('activityID', body.activityID);
   console.log(analytics)
   return analytics
  }
 
-  function fistAccess(body,callback) {
+  function fistAccess(body) {
    const analytics = new AnalyticsSchema()
     analytics.activityID = body.activityID
     analytics.inveniraStdID = body.inveniraStdID
@@ -20,7 +20,7 @@ export default class AnalyticsController {
     analytics.viewModel = false
     analytics.numQueries = 0
     analytics.studentData = []
-    analytics.hash = sha1(body.activityID+body.inveniraStdID)
+    analytics.hash = body.activityID+body.inveniraStdID
     analytics.save()
 }
 
