59ee4908234640ac60561fe59a417ddc1e73d788
WendyNdlalane <wendy.ndlalane@gmail.com>
2023-07-27 23:52:30 +0200

    fix: deployment errors

--- app/Controllers/Http/DeploysController.ts
+++ app/Controllers/Http/DeploysController.ts
@@ -2,20 +2,8 @@
 
 export default class DeploysController {
 
-  const sha1 = require('sha1')
-  const deployRequest = require("./models/deployModel")
 
-function getdeployurl(body,callback) {
-  return
 
-}
-
-function getdeploydata(body,callback){
-
-}
-
-function getstdcode(body,callback){
 
 
 }
-
